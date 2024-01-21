9fd567d35c1c0a21852b72524d6de8bb2f5f4378
TNovosyolova <novoselova.taisia@gmail.com>
2024-01-21 17:30:19 +0300

    HW1: add HW1 files

--- fix_me.py
+++ fix_me.py
@@ -1,8 +1,10 @@
 def calculate_average(nums):
     total = sum(nums)
-    count = len(numbers)
+    count = len(nums)
     average = total / count
+    return average
 
-nums = [10, 15, 20]
-result = calculat_average(nums)
-Print("The average is:", result)
+
+numbers = [10, 15, 20]
+result = calculate_average(numbers)
+print("The average is:", result)

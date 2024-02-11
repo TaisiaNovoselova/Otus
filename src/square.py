8163eb3ae45c05577b9e31f7101b024ad79c4b52
TNovosyolova <novoselova.taisia@gmail.com>
2024-02-11 21:36:00 +0300

    HW2: Delete unused methods

--- src/square.py
+++ src/square.py
@@ -8,12 +8,7 @@ class Square(Rectangle):
             raise ValueError("нельзя создать квадрат")
         super().__init__(side_a, side_a)
 
-    def get_area(self):
-        return self.side_a * self.side_a
-
-    def get_perimetr(self):
-        return 2 * (self.side_a + self.side_a)
-
 
 square = Square(3)
 square.add_area(rectangle)
+
